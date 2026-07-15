e7d761b0402623b9df2974659b026b17241374e6
aleksey <aleshus2007@gmail.com>
2026-07-15 19:42:21 +0300

    updated configs (for "__" change)

--- src/config/db.py
+++ src/config/db.py
@@ -1,14 +1,10 @@
-import os
-
-from dotenv import load_dotenv
 from pydantic import BaseModel
 
-load_dotenv()
 class DBConfig(BaseModel):
-    name: str = os.getenv("DB_NAME")
-    host: str = os.getenv("DB_HOST")
-    port: int = os.getenv("DB_PORT")
-    user: str = os.getenv("DB_USER")
-    password: str = os.getenv("DB_PASSWORD")
-    pool_size: int = os.getenv("DB_POOL_SIZE")
-    max_overflow: int = os.getenv("DB_MAX_OVERFLOW")
+    name: str
+    host: str
+    port: int
+    user: str
+    password: str
+    pool_size: int
+    max_overflow: int
--- src/config/settings.py
+++ src/config/settings.py
@@ -1,11 +1,21 @@
-from pydantic_settings import BaseSettings
-
-from config.db import DBConfig
-from config.app import AppConfig
+# Файл, где собираются ваши Settings
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from src.config.VPNPanelConfig import VPNPanelConfig
+from src.config.db import DBConfig
+from src.config.app import AppConfig
 
 
 class Settings(BaseSettings):
-    db: DBConfig = DBConfig()
-    app: AppConfig = AppConfig()
+    db: DBConfig
+    app: AppConfig = Field(default_factory=AppConfig)
+    vpn_panel: VPNPanelConfig
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__",
+        extra="ignore"
+    )
 
 settings = Settings()
