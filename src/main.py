251cf427ead919b55e69be74ab4e0ed36947b342
aleksey <aleshus2007@gmail.com>
2026-07-15 19:43:32 +0300

    imported inbounds router view

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+from src.api.v1.inbounds import router as inbounds_router
+
+load_dotenv()
 app = FastAPI()
 
+app.include_router(router=inbounds_router)
 
 @app.get("/")
 async def root():
