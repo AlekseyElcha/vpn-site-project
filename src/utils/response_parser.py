b9ea0c7dc9b1f6eca7988f918954db18e92b52b6
aleksey <aleshus2007@gmail.com>
2026-07-16 20:03:22 +0300

    created data extractor from 3x-ui client info response

---
+++ src/utils/response_parser.py
@@ -0,0 +1,26 @@
+from typing import Dict
+
+
+def extract_basic_client_info(
+        data: Dict[str, str]
+):
+    info_block = data.get("obj", {})
+    client_data = info_block.get("client", {})
+
+    used_traffic = info_block.get("usedTraffic")
+    total_traffic = client_data.get("totalGB")
+
+    traffic_left = (total_traffic - used_traffic) if total_traffic > 0 else -1
+
+    total_gb = client_data.get("totalGB") if client_data.get("totalGB") > 0 else -1
+
+    # expiry_time ???
+
+    client_info = {
+        "email": client_data.get("email"),
+        "totalGB": total_gb ,
+        "expiryTime": client_data.get("expiryTime"),
+        "trafficLeft": traffic_left
+    }
+
+    return client_info
