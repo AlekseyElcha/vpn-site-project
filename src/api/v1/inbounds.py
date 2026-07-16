99638736676ea9cf3839ca51c76f4d6ba4c64b90
aleksey <aleshus2007@gmail.com>
2026-07-16 20:05:53 +0300

    improved imports + removed not needed function

--- src/api/v1/inbounds.py
+++ src/api/v1/inbounds.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter
 
-import asyncio
 import aiohttp
 import ssl
 
@@ -30,40 +29,11 @@ async def get_all_inbounds():
     async with aiohttp.ClientSession() as session:
         try:
             async with session.get(url, headers=headers, ssl=ssl_context) as response:
-                print(f"Статус-код: {response.status}")
-                print(url)
-
                 if response.status == 200:
                     data = await response.json()
                     return data
                 else:
                     text = await response.text()
-                    print(f"Ошибка: {text}")
 
         except aiohttp.ClientError as e:
             print(f"Ошибка сети: {e}")
-
-
-@router.get("/{email}")
-async def get_inbounds_by_email(
-        email: str,
-):
-    route = "/inbounds/list"
-    url = f"{base_url}{route}"
-
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(url, headers=headers, ssl=ssl_context) as response:
-                if response.status == 200:
-                    all_inbounds = await response.json()
-                    return all_inbounds
-
-                else:
-                    text = await response.text()
-
-        except aiohttp.ClientError as e:
-            print(e)
