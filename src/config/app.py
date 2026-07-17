7484b7b1141b0142638547e0bebc432cc92e6b09
aleksey <aleshus2007@gmail.com>
2026-07-17 20:38:46 +0300

    updated app config

--- src/config/app.py
+++ src/config/app.py
@@ -1,5 +1,10 @@
+from pathlib import Path
+
 from pydantic import BaseModel
 
-class AppConfig(BaseModel):
-    ...
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+class AppConfig(BaseModel):
+    title: str = "УруруVPN Backend"
+    host: str = "0.0.0.0"
+    port: int = 8000
