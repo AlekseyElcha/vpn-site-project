d9534684788ef7e71f07618379c69d52db4b9a0b
aleksey <aleshus2007@gmail.com>
2026-07-17 20:33:06 +0300

    updated get http session function

--- src/repos/http_connector/get_http_session.py
+++ src/repos/http_connector/get_http_session.py
@@ -1,9 +1,10 @@
+# def get_http_session() -> aiohttp.ClientSession:
+#     if http_session is None:
+#         raise RuntimeError("Сессия aiohttp не инициализирована")
+#     return http_session
+
+from fastapi import Request
 import aiohttp
 
-from src.main import http_session
-
-
-def get_http_session() -> aiohttp.ClientSession:
-    if http_session is None:
-        raise RuntimeError("Сессия aiohttp не инициализирована")
-    return http_session
+def get_http_session(request: Request) -> aiohttp.ClientSession:
+    return request.app.state.http_session
