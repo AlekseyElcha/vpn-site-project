aed1bdf9c3bf4df5a8b8aca0ce92c2110e049b94
aleksey <aleshus2007@gmail.com>
2026-07-17 20:26:36 +0300

    added tg bot & jwt configs in settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,6 +1,8 @@
-# Файл, где собираются ваши Settings
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from src.config.jwt import AuthJWT
+from src.config.bot import TgBotConfig
 from src.config.VPNPanelConfig import VPNPanelConfig
 from src.config.db import DBConfig
 from src.config.app import AppConfig
@@ -10,6 +12,8 @@ class Settings(BaseSettings):
     db: DBConfig
     app: AppConfig = Field(default_factory=AppConfig)
     vpn_panel: VPNPanelConfig
+    bot: TgBotConfig
+    jwt: AuthJWT = Field(default_factory=AuthJWT)
 
     model_config = SettingsConfigDict(
         env_file=".env",
