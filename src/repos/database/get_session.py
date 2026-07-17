84e3769ad95e325f43f62717c6d460f867cfe332
aleksey <aleshus2007@gmail.com>
2026-07-17 20:32:36 +0300

    renamed get db session function: get_session -> get_db_session

--- src/repos/database/get_session.py
+++ src/repos/database/get_session.py
@@ -5,6 +5,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from src.repos.database.session_factory import async_session_maker
 
 
-async def get_session() -> AsyncGenerator[AsyncSession, None]:
+async def get_db_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
