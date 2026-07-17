47b0d0cd249b2694bc3dab0a13bfd9103e7c44e3
aleksey <aleshus2007@gmail.com>
2026-07-17 20:29:10 +0300

    created create_new_vpn_client function

--- src/core/clients/new_client.py
+++ src/core/clients/new_client.py
@@ -1,14 +1,46 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+import ssl
 
+import aiohttp
+from aiohttp.pytest_plugin import AiohttpClient
+
+from src.exceptions.x_ui_exception_handler import ThreeXUIExceptionHandler
+from src.config.settings import settings
 from src.dtos.schemas import NewClientSchema
-from src.repos.database.crud.creation import add_new_client_to_db
 
-async def create_new_client(
+headers = \
+    {
+        "Authorization": f"Bearer {settings.vpn_panel.auth_token}",
+        "Accept": "application/json"
+    }
+
+base_url = settings.vpn_panel.panel_url
+
+
+async def create_new_vpn_client(
         new_client: NewClientSchema,
-        session: AsyncSession
+        session: aiohttp.ClientSession
 ):
-    await add_new_client_to_db(
-        new_client=new_client,
-        session=session
-    )
-    return 1
+    route = f"/clients/add"
+    url = f"{base_url}{route}"
+
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
+    client_data = new_client.model_dump(by_alias=True)
+
+    try:
+        async with session.post(url, headers=headers, json=client_data, ssl=ssl_context) as response:
+            if response.status in (200, 201):
+                data = await response.json()
+
+                ThreeXUIExceptionHandler.handle_response(data)
+                return data
+
+            text = await response.text()
+            print(text)
+
+
+    except aiohttp.ClientError as e:
+        print(f"Ошибка HTTP-запроса: {e}")
+
