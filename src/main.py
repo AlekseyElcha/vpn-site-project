42112298e037d10e6cf6b860461c3c891081d5cd
aleksey <aleshus2007@gmail.com>
2026-07-18 20:08:56 +0300

    included payment api router

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from src.fastapi_startup import start
 from src.api.v1.inbounds import router as inbounds_router
 from src.api.v1.clients import router as clients_router
 from src.auth.autherization import router as auth_router
+from src.payments.pay import router as payment_router
 
 
 @asynccontextmanager
@@ -41,6 +42,7 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(router=inbounds_router)
 app.include_router(router=clients_router)
 app.include_router(router=auth_router)
+app.include_router(router=payment_router)
 
 @app.get("/")
 async def root():
