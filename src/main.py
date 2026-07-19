fd67a32966c8576f1a01935cbf58df12f871037d
aleksey <aleshus2007@gmail.com>
2026-07-19 21:37:36 +0300

    added description for tags + included users_router (user views)

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from src.fastapi_startup import start
 from src.api.v1.inbounds import router as inbounds_router
 from src.api.v1.clients import router as clients_router
+from src.api.v1.users import router as users_router
 from src.auth.autherization import router as auth_router
 from src.payments.pay import router as payment_router
 
@@ -20,12 +21,40 @@ async def lifespan(_: FastAPI):
     print("started")
 
 
-app = FastAPI(lifespan=lifespan)
+tags_metadata = [
+    {
+        "name": "Inbounds",
+        "description": "Получение данных по инбаундам VPN-сервера.",
+    },
+    {
+        "name": "Clients",
+        "description": "Работа с VPN-клиентами (подписками).",
+    },
+    {
+        "name": "Users",
+        "description": "Работа с пользователями VPN-сервиса.",
+    },
+    {
+        "name": "Authentication",
+        "description": "Работа с авторизацией в VPN-сервисе.",
+    },
+    {
+        "name": "Payment",
+        "description": "Оплата услуг с использованием Telegram Stars",
+    },
+]
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    openapi_tags=tags_metadata
+)
 
 app.include_router(router=inbounds_router)
 app.include_router(router=clients_router)
 app.include_router(router=auth_router)
 app.include_router(router=payment_router)
+app.include_router(router=users_router)
 
 @app.get("/")
 async def root():
