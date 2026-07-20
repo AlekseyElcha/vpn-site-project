ea9c329c5c2e7b31b60924dac815057547685bf5
aleksey <aleshus2007@gmail.com>
2026-07-20 13:09:42 +0300

    created /subs endpoint

--- src/api/v1/users.py
+++ src/api/v1/users.py
@@ -1,8 +1,11 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.params import Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.responses import JSONResponse
 
+from src.core.clients.client_info import get_subscriptions_by_tg_id
 from src.exceptions.db import DBCrudException
 from src.repos.database.crud.basic_utils import get_user_balance_by_tg_id
 from src.repos.database.get_session import get_db_session
@@ -34,5 +37,22 @@ async def get_user_balance(
         )
 
 
+
+@router.get("/subs")
+async def get_user_subscriptions(
+        tg_id: int = Query(...),
+        db_session: AsyncSession = Depends(get_db_session)
+):
+    try:
+        results = await get_subscriptions_by_tg_id(
+            tg_id=tg_id,
+            session=db_session
+        )
+        return results
+    except DBCrudException:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Внутренняя ошибка сервера."
+        )
 
 
